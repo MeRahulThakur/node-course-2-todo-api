72a6e2ce6451987f8162da2c25b30695e48664f7
RAHUL THAKUR <iamrahulthakur.1992@gmail.com>
2018-03-05 23:16:22 +0530

    Add tests fo POST /users/login

--- server/tests/seed/seed.js
+++ server/tests/seed/seed.js
@@ -17,10 +17,10 @@ const users = [{
   _id: userTwoId,
   email: 'rahulthakur@gmail.com',
   password: 'userTwoPass',
-  tokens: [{
-      access: 'auth',
-      token: jwt.sign({_id: userTwoId, access: 'auth'},'abc123').toString()
-  }]
+  // tokens: [{
+  //     access: 'auth',
+  //     token: jwt.sign({_id: userTwoId, access: 'auth'},'abc123').toString()
+  // }]
 }];
 
 const todos = [{
